c5a35620bf27b799e8156f46c580d3b11e984397
Andrew Ry <RyAndrew@users.noreply.github.com>
2019-06-29 20:32:12 -0500

    cleanup

--- proxy101.php
+++ proxy101.php
@@ -8,26 +8,16 @@ if($_SERVER['REMOTE_ADDR'] !== 'jonsiphere' && !(isset($_SERVER['HTTP_REFERER'])
 }
 setcookie('joke',1,time()+60*60*24*365,'/');
 
-$ch = curl_init(); 
-
-curl_setopt($ch, CURLOPT_URL, "https://www.101soundboards.com" . $_SERVER['REQUEST_URI']); 
-
+$ch = curl_init("https://www.101soundboards.com" . $_SERVER['REQUEST_URI']); 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); 
 curl_setopt($ch, CURLOPT_HEADER, 1);
-
 if(FALSE === $response = curl_exec($ch)){
 	die("fart");
 }
-
 $header_size = curl_getinfo($ch, CURLINFO_HEADER_SIZE);
 $headerSection = substr($response, 0, $header_size);
-
 $body = substr($response, $header_size);
-
-// close curl resource to free up system resources 
-curl_close($ch);      
-
-//var_dump($headerSection);
+curl_close($ch);
 
 $headersLines = explode("\r\n",$headerSection);
 $headersArray = array();
@@ -37,22 +27,27 @@ foreach($headersLines as $header){
 	}
 	$headersArray[strtoupper(substr($header,0,$colonPos))] = trim(substr($header,$colonPos+1));
 }
-//var_dump($headersArray);
-//exit;
+
 if(isset($headersArray['CONTENT-TYPE'])){
 	header('Content-Type: '.$headersArray['CONTENT-TYPE']);
 }
-//
+
+//monkey with html
 if($_SERVER['REQUEST_URI'] === '/' || (isset($headersArray['CONTENT-TYPE']) && substr($headersArray['CONTENT-TYPE'],0,9) === 'text/html') ){
 
+	//logo
 	$body = str_replace('<img src="/img/101soundsboards-logo.svg" width="303" height="50" alt="101soundboards.com" />','101fartboards.com', $body);
-	//$body = str_ireplace('101soundboards','101fartboards', $body);
+
+	//logo link
+	$body = str_ireplace('101soundboards.com','101soundboard.com', $body);
+
+	//fun
 	$body = str_ireplace(' sound',' fart', $body);
 
+	//fix broken
 	$body = str_ireplace(', fart)',', sound)', $body);
 	$body = str_ireplace('fart.board','sound.board', $body);
 	$body = str_ireplace('fart.id','sound.id', $body);
-	
 	
 }
 
